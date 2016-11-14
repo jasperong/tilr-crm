cff03930d5e128b52f246b113d055f5fe08ead7a
Jasper <jave.ong4@yahoo.com>
2016-11-14 14:11:41 -0500

    Style forms, remove unnecessary views

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -3,44 +3,45 @@ require 'test_helper'
 class ContactsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @contact = contacts(:one)
+    @user = users(:one)
   end
 
   test "should get index" do
-    get contacts_url
+    get user_contacts_url(@user)
     assert_response :success
   end
 
   test "should get new" do
-    get new_contact_url
+    get new_user_contact_url(@user)
     assert_response :success
   end
 
   test "should create contact" do
     assert_difference('Contact.count') do
-      post contacts_url, params: { contact: { company: @contact.company, email: @contact.email, first_name: @contact.first_name, last_name: @contact.last_name, phone_number: @contact.phone_number, user_id: @contact.user_id } }
+      post user_contacts_url(@user), params: { contact: { company: @contact.company, email: @contact.email, first_name: @contact.first_name, last_name: @contact.last_name, phone_number: @contact.phone_number, user_id: @contact.user_id } }
     end
 
-    assert_redirected_to contact_url(Contact.last)
+    assert_redirected_to user_contact_url(Contact.last)
   end
 
   test "should show contact" do
-    get contact_url(@contact)
+    get user_contact_url(@user,@contact)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_contact_url(@contact)
+    get edit_user_contact_url(@user, @contact)
     assert_response :success
   end
 
   test "should update contact" do
-    patch contact_url(@contact), params: { contact: { company: @contact.company, email: @contact.email, first_name: @contact.first_name, last_name: @contact.last_name, phone_number: @contact.phone_number, user_id: @contact.user_id } }
-    assert_redirected_to contact_url(@contact)
+    patch user_contact_url(@user, @contact), params: { contact: { company: @contact.company, email: @contact.email, first_name: @contact.first_name, last_name: @contact.last_name, phone_number: @contact.phone_number, user_id: @contact.user_id } }
+    assert_redirected_to user_contact_url(@user, @contact)
   end
 
   test "should destroy contact" do
     assert_difference('Contact.count', -1) do
-      delete contact_url(@contact)
+      delete user_contact_url(@user, @contact)
     end
 
     assert_redirected_to contacts_url
