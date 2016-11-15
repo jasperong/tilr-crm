75ef607a9feed1a91c1abde7f251ce877d9825c6
Jasper <jave.ong4@yahoo.com>
2016-11-15 00:12:07 -0500

    Sign up/ log in from google oauth working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '/', controllers: { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, path: '/', controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
 
   resources :users, only: [] do
