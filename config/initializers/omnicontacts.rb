2b589ccad154ad76715499920cb35e7535924949
Jasper <jave.ong4@yahoo.com>
2016-11-16 12:50:58 -0500

    Testing fb oauth

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -2,4 +2,5 @@ require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
   importer :gmail, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  importer :facebook, ENV["FB_CLIENT_ID"], ENV["FB_CLIENT_SECRET"]
 end
